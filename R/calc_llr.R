4dc5b2a5f0e74e53b850275df3ee01712db07a8d
entjos <josent@kep.local>
2023-10-10 11:39:52 +0200

    Added description for internal functions

--- R/calc_llr.R
+++ R/calc_llr.R
@@ -1,3 +1,22 @@
+#' Calculation of the log-likelihood ratio for `TreeScan`
+#'
+#' This is an internal function requiered for `TreeScan`.
+#'
+#' @noRd
+#'
+#' @param counts a data.table created with `get_cuts`
+#'
+#' @param no_iteration the number of the Monte-Carlo iteration
+#'
+#' @param p the expected proportion of unexposed individuals
+#'
+#' @return a data.table with the following columns
+#'    \item{no_iteration}{The number of the Monte-Carlo iteration}
+#'    \item{cut}{The name of the cut}
+#'    \item{n0}{The number of events amogn unexposed individuals}
+#'    \item{n1}{The number of events among the exposed individuals}
+#'    \item{llr}{The log-likelihood ratio}
+
 calc_llr <- function(counts, no_iteration, p){
 
   n0 <- n1 <- q0 <- q1 <- ll0 <- lla <- llr <- iteration <- NULL
--- R/get_cuts.R
+++ R/get_cuts.R
@@ -1,3 +1,24 @@
+#' Obtaining the number of events in different cuts for `TreeScan`
+#'
+#' This is an internal function requiered for `TreeScan`.
+#'
+#' @noRd
+#'
+#' @param data a data.table passed to TreeScan
+#'
+#' @param id name of the `id` variable passed to TreeScan
+#'
+#' @param exposure name of the `exposure` variable passed to TreeScan
+#'
+#' @param leafs name of the `leafs` variable passed to TreeScan
+#'
+#' @param n_char the number of letters considered for the cut
+#'
+#' @return a data.table with the following columns
+#'    \item{cut}{The name of the cut}
+#'    \item{n0}{The number of events belonging to the cut among unexposed individuals}
+#'    \item{n1}{The number of events belonging to the cut among exposed individuals}
+
 get_cuts <- function(data,
                      id,
                      exposure,
