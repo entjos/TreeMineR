33de0c7e274667e880936e5df9d77349793c7991
Joshua Entrop <josent@kep.local>
2023-10-11 11:31:47 +0200

    Fixed error in calculation of llr

--- R/calc_llr.R
+++ R/calc_llr.R
@@ -21,12 +21,12 @@ calc_llr <- function(counts, no_iteration, p){
 
   n0 <- n1 <- q0 <- q1 <- ll0 <- lla <- llr <- iteration <- NULL
 
-  counts[, q0 := n0 /(n0 + n1)]
   counts[, q1 := n1 /(n0 + n1)]
+  counts[, q0 := n0 /(n0 + n1)]
 
   counts[, lla := log(q1) * n1 + log(q0) * n0]
   counts[, ll0 := log(p)  * n1 + log(1 - p)  * n0]
-  counts[, llr := lla - ll0]
+  counts[, llr := (lla - ll0) * as.numeric(q1 > p)]
 
   counts[, list(iteration = no_iteration, cut, n0, n1, llr)]
 
