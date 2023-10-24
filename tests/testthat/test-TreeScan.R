1cbb02017e3be77272ee3365100c794bd3a993d7
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-24 16:47:57 +0200

    Added new tests for new error message and cut_positions argument

--- tests/testthat/test-TreeScan.R
+++ tests/testthat/test-TreeScan.R
@@ -23,3 +23,48 @@ test_that("Check parallel test run", {
   })
 })
 
+test_that("Check user defined cut points", {
+  expect_equal({
+    temp <- TreeScan(diagnoses,
+                     exposure = case,
+                     leafs = diag,
+                     id = id,
+                     cut_positions = c(1, 3),
+                     random_seed = 1234,
+                     n_monte_carlo_sim = 20,
+                     future_control = list("multisession", workers = 2))
+
+    unique(nchar(temp$cut))
+  }, c(1, 3))
+})
+
+test_that("Check user defined cut points in random order",{
+  expect_equal({
+    temp <- TreeScan(diagnoses,
+                     exposure = case,
+                     leafs = diag,
+                     id = id,
+                     cut_positions = c(3, 1, 4),
+                     random_seed = 1234,
+                     n_monte_carlo_sim = 20,
+                     future_control = list("multisession", workers = 2))
+
+    unique(nchar(temp$cut))
+  }, c(1, 3, 4))
+})
+
+test_that("Defining cuts longer than max(nchar(leaf)) causes an error",{
+  expect_error({
+    TreeScan(diagnoses,
+             exposure = case,
+             leafs = diag,
+             id = id,
+             cut_positions = c(3, 1, 5),
+             random_seed = 1234,
+             n_monte_carlo_sim = 20,
+             future_control = list("multisession", workers = 2))
+  }, regexp = "Your cut position\\(s\\) 5")
+})
+
+
+
