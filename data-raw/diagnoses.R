66f8240d86bdb0ba2b0c0111b4ea1ceadef83737
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-30 15:46:52 +0100

    Exclude icd codes not included in icd-10-se from diagnoses data set

--- data-raw/diagnoses.R
+++ data-raw/diagnoses.R
@@ -6,22 +6,33 @@
 #' @format `diagnoses`
 #' A data frame with 23,144 rows and 3 columns
 #' \describe{
-#'    \item{id}{Individual identifier}
-#'    \item{case}{Indicator for case status}
-#'    \item{diag}{An ICD-10 diagnosis code}
+#'    \item{`leaf`}{The leaf code}
+#'    \item{`n0`}{The number of unexposed individuals on this leaf}
+#'    \item{`n1`}{The number of exposed individuals on this leaf
 #' }
 #'
 #' @keywords data
 set.seed(1)
 
-comp <- data.frame(id = rep(1:10000, rbinom(10000, 10, 0.2)))
+comp <- data.table::data.table(id = rep(1:10000, rbinom(10000, 10, 0.2)))
 comp$diag <- comorbidity::sample_diag(n = nrow(comp))
 comp$case <- 0
 
-cases <- data.frame(id = rep(1:1000, rbinom(1000, 10, 0.3)))
+cases <- data.table::data.table(id = rep(1:1000, rbinom(1000, 10, 0.3)))
 cases$diag <- comorbidity::sample_diag(n = nrow(cases))
 cases$case <- 1
 
 diagnoses <- rbind(comp, cases)
+
+# Remove codes that are not included on in the ICD-10-SE
+diagnoses <- diagnoses[diagnoses$diag %in% gsub(paste0(".*(?<=\\/)(.*)"), "\\1",
+                                                icd_10_se$pathString,
+                                                perl = TRUE), ]
+
+colnames(diagnoses) <- c("id", "leaf", "case")
+
+diagnoses<- diagnoses[, .(n0 = sum(case == 0),
+                          n1 = sum(case == 1)),
+                      by = leaf]
 
 usethis::use_data(diagnoses, overwrite = TRUE)
