2ba49509d22840a96a9278627ca95d0afabc2653
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-30 15:46:06 +0100

    Added icd-10-se tree

---
+++ R/icd_10_se.R
@@ -0,0 +1,15 @@
+#' Swedish version of the ICD-10 diagnoses code tree
+#'
+#' A dataset including the following columns:
+#' \itemize{
+#'    \item{`pathString`}{A string identifying all the parents of a node. Each
+#'    parent is separated by a `/`.}
+#'    \item{`child`}{The child node of the `stringPath`.}
+#'    \item{`parent<i>`}{The ith parent of the `child`.}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name icd_10_se
+#' @usage data(icd_10_se)
+NULL
---
+++ data-raw/icd_10_se.R
@@ -0,0 +1,18 @@
+library(data.table)
+
+icd_10_codes <- icd_10_se <- data.table::fread(
+  paste0("https://www.socialstyrelsen.se/globalassets",
+         "/sharepoint-dokument/dokument-webb/",
+         "klassifikationer-och-koder/icd-10-se.tsv")
+)
+
+icd_10_codes <- icd_10_codes[, .(node   = Kod,
+                                 parent = `Överordnad kod`)]
+icd_10_codes[icd_10_codes == ""] <- NA
+icd_10_codes <- tidyr::fill(icd_10_codes, parent, .direction = "down")
+icd_10_codes <- unique(icd_10_codes, by = c("node", "parent"))
+icd_10_codes <- icd_10_codes[, lapply(.SD, function(x) gsub("\\.", "", x))]
+
+icd_10_se <- create_tree(icd_10_codes)
+
+usethis::use_data(icd_10_se, overwrite = TRUE)
