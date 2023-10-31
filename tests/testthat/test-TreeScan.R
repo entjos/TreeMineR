7a837b74b634a3f3790997ad6436722500bc78a3
Joshua Entrop <entjos@users.noreply.github.com>
2023-10-31 09:25:49 +0100

    Correct expected p in test-TreeScan

--- tests/testthat/test-TreeScan.R
+++ tests/testthat/test-TreeScan.R
@@ -2,7 +2,7 @@ test_that("Check sequential test run", {
   expect_snapshot({
     TreeScan(count = diagnoses,
              tree  = icd_10_se,
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 10,
              random_seed = 1234) |>
       head(10)
@@ -10,13 +10,14 @@ test_that("Check sequential test run", {
 })
 
 test_that("Check parallel test run", {
-  expect_no_error({
+  expect_snapshot({
     TreeScan(count = diagnoses,
              tree  = icd_10_se,
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 20,
              random_seed = 124,
-             future_control = list("multisession", workers = 2))
+             future_control = list("multisession", workers = 2)) |>
+      head(10)
   })
 })
 
@@ -24,7 +25,7 @@ test_that("Test that all leafs are included on your tree",{
   expect_error({
     TreeScan(count = data.frame(leaf = "KLM", n1 = 5, n0 = 2),
              tree  = icd_10_se,
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 10,
              random_seed = 1234)
   }, regexp = "The following leafs are not included on your tree: KLM")
@@ -34,7 +35,7 @@ test_that("Test no clumn called leaf exisits in tree",{
   expect_error({
     TreeScan(count = data.frame(leaf = "KLM", n1 = 5, n0 = 2),
              tree  = data.frame(leaf = "KLM", pathString = "1/KLM"),
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 10,
              random_seed = 1234)
   }, regexp = "includes a column named `leaf`, which is reserved by TreeScan.")
@@ -44,7 +45,7 @@ test_that("Test no clumn pathString exisits in tree",{
   expect_error({
     TreeScan(count = data.frame(leaf = "KLM", n1 = 5, n0 = 2),
              tree  = data.frame(path = "1/KLM"),
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 10,
              random_seed = 1234)
   }, regexp = "Could not find column `pathString` in `tree`")
@@ -54,7 +55,7 @@ test_that("Test no clumn pathString exisits in tree",{
   expect_error({
     TreeScan(count = data.frame(leaf = "KLM", n1 = 5, n0 = 2),
              tree  = data.frame(pathString = "1-KLM"),
-             p = 1/2,
+             p = 1/11,
              n_monte_carlo_sim = 10,
              random_seed = 1234)
   }, regexp = "I could not any match for / in `pathString`.")
