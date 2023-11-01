af4e1c1ae5e596ab7aa43ef2a68efef7d8b30663
Joshua Entrop <entjos@users.noreply.github.com>
2023-11-01 09:48:58 +0100

    Add test for error message in create_tree

--- tests/testthat/test-create_tree.R
+++ tests/testthat/test-create_tree.R
@@ -5,4 +5,9 @@ test_that("Check that correctness of tree",
             }, c("01", "01/A", "01/A/AB"))
           })
 
-
+test_that("Check that correctness of tree",
+          {expect_error({
+            create_tree(data.frame(node  = c("01", "A", "AB"),
+                                   parent = c("", "01", "A")))
+          }, "`node` and/or `parent` include empty cells")
+          })
