abc407ce60e590f38e3c52767bbcfc994c336fd6
Joshua Entrop <entjos@users.noreply.github.com>
2023-12-06 10:36:01 +0100

    Remove description in diagnosis.R raw file

--- data-raw/diagnoses.R
+++ data-raw/diagnoses.R
@@ -1,17 +1,3 @@
-#' Test dataset of ICD diagnoses
-#'
-#' A simulated dataset of hospital diagnoses created with the help of
-#' the `comorbidity` package.
-#'
-#' @format `diagnoses`
-#' A data frame with 23,144 rows and 3 columns
-#' \describe{
-#'    \item{`leaf`}{The leaf code}
-#'    \item{`n0`}{The number of unexposed individuals on this leaf}
-#'    \item{`n1`}{The number of exposed individuals on this leaf
-#' }
-#'
-#' @keywords data
 set.seed(1)
 
 unexposed <- data.table::data.table(id = rep(1:10000, rbinom(10000, 10, 0.2)))
