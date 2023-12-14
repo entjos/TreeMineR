53999bf2df8170ee44adb1fb1d123039bfac0bf6
Joshua Entrop <entjos@users.noreply.github.com>
2023-12-14 15:19:24 +0100

    Fix error when adding ICD-10-SE as parent to the ICD tree file

--- data-raw/icd_10_se.R
+++ data-raw/icd_10_se.R
@@ -1,18 +1,31 @@
 library(data.table)
 
+# Load ICD-10-SE codes from SoS
 icd_10_codes <- icd_10_se <- data.table::fread(
   paste0("https://www.socialstyrelsen.se/globalassets",
          "/sharepoint-dokument/dokument-webb/",
          "klassifikationer-och-koder/icd-10-se.tsv")
 )
 
+# Rename variables
 icd_10_codes <- icd_10_codes[, .(node   = Kod,
                                  parent = `Överordnad kod`)]
-icd_10_codes[icd_10_codes == ""] <- "ICD-10-SE"
+
+# Replace NA codes with the code above (due to the data structure)
+icd_10_codes[icd_10_codes == ""] <- NA
 icd_10_codes <- tidyr::fill(icd_10_codes, parent, .direction = "down")
+
+# Remove duplicates
 icd_10_codes <- unique(icd_10_codes, by = c("node", "parent"))
+
+# Remove dotes, e.g. Z99.2 -> Z999
 icd_10_codes <- icd_10_codes[, lapply(.SD, function(x) gsub("\\.", "", x))]
 
+# Add ICD-10-SE as highest level code
+icd_10_codes[1:22, parent := "ICD-10-SE"]
+
+# Create tree string
 icd_10_se <- create_tree(icd_10_codes)
 
+# Add data to package
 usethis::use_data(icd_10_se, overwrite = TRUE)
--- tests/testthat/_snaps/drop_cuts.md
+++ tests/testthat/_snaps/drop_cuts.md
@@ -3,11 +3,11 @@
     Code
       head(drop_cuts(tree = icd_10_se, cuts = c("B35-B49", "F41"), return_removed = FALSE))
     Output
-        pathString
-      1         01
-      2         02
-      3         03
-      4         04
-      5         05
-      6         06
+                  pathString
+      1 ICD-10-SE/01/A00-A09
+      2 ICD-10-SE/01/A15-A19
+      3 ICD-10-SE/01/A20-A28
+      4 ICD-10-SE/01/A30-A49
+      5 ICD-10-SE/01/A50-A64
+      6 ICD-10-SE/01/A65-A69
 
