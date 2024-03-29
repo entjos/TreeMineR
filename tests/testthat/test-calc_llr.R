17691f859916189534c0d04f4f1488d112f8ad03
Michael Chirico <michaelchirico4@gmail.com>
2024-03-29 12:12:47 -0700

    Switch data.table to Imports:

--- tests/testthat/test-calc_llr.R
+++ tests/testthat/test-calc_llr.R
@@ -1,7 +1,7 @@
 test_that("LLR is close to 0 if q0 = p",{
   expect_equal({
 
-    calc_llr(data.table(cut = "AA1", n0 = 230, n1 = 23),
+    calc_llr(data.table::data.table(cut = "AA1", n0 = 230, n1 = 23),
              no_iteration = 1, p = 0.1)[["llr"]]
 
   }, 0)
